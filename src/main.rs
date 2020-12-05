fe8372f8dfed72cd1f70392089c944d01cbcfe9b
bulengerkacper <bulenger.kacper@gmail.com>
2020-12-05 16:08:52 +0100

    i lost 3hrs of work

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,11 @@ mod world;
 use crate::world::generator::Element;
 use crate::world::generator::Map;
 use crate::world::generator::Visibility;
+use crate::world::graphic::VisualEngine;
+
 
 fn main() {
     let mapka: Map = world::generator::Map::create(900,900,900);
     mapka.make_visible(true);
-    world::graphic::window_maker();
+    world::graphic::VisualEngine::window_maker("autokilah", kiss3d::light::Light::StickToCamera);
 }
--- src/world/graphic.rs
+++ src/world/graphic.rs
@@ -2,31 +2,39 @@ use kiss3d;
 use nalgebra as na;
 
 use kiss3d::light::Light;
+use kiss3d::scene::SceneNode;
 use kiss3d::window::Window;
 use na::{UnitQuaternion, Vector3};
 
-pub fn window_maker() {
-    let mut window = Window::new("Auto killah");
-    window.set_background_color(0.0,0.0,0.0);
-    let mut main_cube = window.add_cube(0.4,0.2,0.5);
-    main_cube.set_color(0.0,102.0,0.0);
-    window.set_light(Light::StickToCamera);
+pub struct VisualEngine {}
 
-    let rot = UnitQuaternion::from_axis_angle(&Vector3::y_axis(), 0.014);
-
-    while window.render() {
-        main_cube.prepend_to_local_rotation(&rot);
+impl VisualEngine {
+    pub fn window_maker(name: &str, x: kiss3d::light::Light) -> Window {
+        let mut window = Window::new(name);
+        window.set_light(x);
+        while window.render() {}
+        window
     }
-}
 
-pub fn add_rotation_in_x() {
-
-}
-
-pub fn add_rotation_in_y() {
+    pub fn change_background_color(wind:&mut Window, r: f32 ,g: f32,b: f32) {
+        wind.set_background_color(r,g,b);
+    }
 
 }
 
-pub fn add_rotation_in_z() {
-    
+trait Move {
+    fn add_rotation_in_axis(&mut self, speed: f32, axis: char);
+}
+
+impl Move for SceneNode {
+    fn add_rotation_in_axis(&mut self, speed: f32, axis: char) {
+        let rot;
+        match axis {
+            'x' => rot = UnitQuaternion::from_axis_angle(&Vector3::x_axis(), speed),
+            'y' => rot = UnitQuaternion::from_axis_angle(&Vector3::y_axis(), speed),
+            'z' => rot = UnitQuaternion::from_axis_angle(&Vector3::z_axis(), speed),
+            _ => return,
+        }
+        self.prepend_to_local_rotation(&rot);
+    }
 }
