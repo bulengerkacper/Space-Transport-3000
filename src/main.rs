9e61c203c708707c1fe3422e115940eefe2b36eb
bulengerkacper <bulenger.kacper@gmail.com>
2020-12-06 14:16:39 +0100

    extracted spaceship to other fn

--- src/main.rs
+++ src/main.rs
@@ -19,22 +19,26 @@ fn main() {
         0.0,
     );
 
+    create_spaceship(&mut window);
+    generate_plantes(100, &mut window);
+    while window.render() {
+        // sphere.add_rotation_in_axis(0.01, 'x');
+        // sphere.add_rotation_in_axis(0.01, 'y');
+    }
+}
+
+fn create_spaceship(window: &mut Window) {
     let space_ship_obj = Path::new("./src/resources/spaceship/statek.obj");
     let mut space_ship = window.add_obj(
         &space_ship_obj,
         &space_ship_obj,
         Vector3::new(0.2, 0.2, 0.2),
     );
-    space_ship.append_translation(&Translation3::new(0.0,0.0,3.0));
-    space_ship.append_translation(&Translation3::new(0.0,-1.0,0.0));
-    space_ship.add_rotation_in_axis(3.15,'y');
-    let texture_1 = include_bytes!("./resources/textures/sphere_texture.png");
-    space_ship.set_texture_from_memory(texture_1, "texture_1");
-    generate_plantes(50, &mut window);
-    while window.render() {
-        // sphere.add_rotation_in_axis(0.01, 'x');
-        // sphere.add_rotation_in_axis(0.01, 'y');
-    }
+    space_ship.append_translation(&Translation3::new(0.0, 0.0, 4.0));
+    space_ship.append_translation(&Translation3::new(0.0, -1.5, 0.0));
+    space_ship.add_rotation_in_axis(3.15, 'y');
+    let metal_texture = include_bytes!("./resources/textures/metal_texture.png");
+    space_ship.set_texture_from_memory(metal_texture, "metal_texture");
 }
 
 fn generate_plantes(planet_number: u32, window: &mut Window) {
@@ -43,11 +47,11 @@ fn generate_plantes(planet_number: u32, window: &mut Window) {
 
     while n < planet_number {
         let mut sphere = window.add_sphere(0.1);
-        let texture_1 = include_bytes!("./resources/textures/sphere_texture.png");
-        sphere.set_texture_from_memory(texture_1, "texture_1");
+        let sphere_texture = include_bytes!("./resources/textures/sphere_texture.png");
+        sphere.set_texture_from_memory(sphere_texture, "sphere_texture");
         sphere.append_translation(&Translation3::new(
             rng.gen_range(-15.0, 15.0),
-            rng.gen_range(-5.0, 5.0),
+            rng.gen_range(-10.0, 10.0),
             rng.gen_range(-15.0, 15.0),
         ));
         n += 1;
