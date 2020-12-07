075fe9653185ba089c47b22cd036e74cb470bbeb
bulengerkacper <bulenger.kacper@gmail.com>
2020-12-07 23:45:34 +0100

    renaming

--- src/main.rs
+++ src/main.rs
@@ -10,24 +10,23 @@ use na::{Point2, Point3};
 use rand::Rng;
 
 fn main() {
-    let mut window = ve::create_window(
+    let mut game_window = ve::create_window(
         "Space killer",
         kiss3d::light::Light::StickToCamera,
         0.0,
         0.0,
         0.0,
     );
-
-    let font = Font::default();
-    let mut space_ship = generator::create_spaceship(&mut window);
-    let mut planets = generator::generate_plantes(30, &mut window);
+    let mut space_ship = generator::create_spaceship(&mut game_window);
+    let mut planets = generator::generate_plantes(30, &mut game_window);
     let mut rng = rand::thread_rng();
     let mut speed_of_plantes: f32 = 0.00;
-    while window.render() {
+    let font = Font::default();
+    while game_window.render() {
         for planet in &mut planets {
             planet.add_rotation_in_axis(rng.gen_range(0.0, 0.01), 'y');
         }
-        for mut event in window.events().iter() {
+        for mut event in game_window.events().iter() {
             match event.value {
                 WindowEvent::Key(Key::W, Action::Press, _) => {
                     space_ship.move_obj(0.0, 0.05, 0.0);
@@ -56,7 +55,7 @@ fn main() {
             speed_of_plantes.abs()
         );
         let pts: &str = &points[..];
-        window.draw_text(
+        game_window.draw_text(
             pts,
             &Point2::new(43.0, 43.0),
             90.0,
@@ -65,7 +64,7 @@ fn main() {
         );
         if generator::move_planets(&space_ship, &mut planets, speed_of_plantes) {
             speed_of_plantes = 0.0;
-            window.draw_text(
+            game_window.draw_text(
                 "YOU LOSE!\n MOVE TO RESTART\n L to LEAVE",
                 &Point2::new(600.0, 600.0),
                 120.0,
