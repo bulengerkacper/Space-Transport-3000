8919dd06cee342ac95ad17ed03627d1b3aed15f3
bulengerkacper <bulenger.kacper@gmail.com>
2020-12-07 12:02:39 +0100

    added events

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ extern crate nalgebra as na;
 use crate::world::generator;
 use crate::world::graphic::Move;
 use crate::world::graphic::VisualEngine as ve;
+use kiss3d::event::{Action, Key, WindowEvent};
+
 use rand::Rng;
 
 fn main() {
@@ -15,12 +17,35 @@ fn main() {
         0.0,
     );
 
-    let _space_ship = generator::create_spaceship(&mut window);
+    let mut space_ship = generator::create_spaceship(&mut window);
     let mut planets = generator::generate_plantes(40, &mut window);
     let mut rng = rand::thread_rng();
+    // generator::move_spaceship(&mut space_ship,0.0,0.0,0.0);
     while window.render() {
         for planet in &mut planets {
-            planet.add_rotation_in_axis(rng.gen_range(0.0, 0.2), 'x');
+            planet.add_rotation_in_axis(rng.gen_range(0.0, 0.01), 'y');
+        }
+        for mut event in window.events().iter() {
+            match event.value {
+                WindowEvent::Key(Key::W, Action::Press, _) => {
+                    generator::move_spaceship(&mut space_ship, 0.0, 0.05, 0.0);
+                }
+                WindowEvent::Key(Key::S, Action::Press, _) => {
+                    generator::move_spaceship(&mut space_ship, 0.0, -0.05, 0.0);
+                }
+                WindowEvent::Key(Key::A, Action::Press, _) => {
+                    generator::move_spaceship(&mut space_ship, 0.05, 0.0, 0.0);
+                }
+                WindowEvent::Key(Key::D, Action::Press, _) => {
+                    generator::move_spaceship(&mut space_ship, -0.05, 0.0, 0.0);
+                }
+                WindowEvent::MouseButton(button, Action::Release, mods) => {
+                    println!("You released the mouse button: {:?}", button);
+                    println!("You released the mouse button with modifiers: {:?}", mods);
+                    // dont override the default mouse handler
+                }
+                _ => {}
+            }
         }
     }
 }
--- src/world/generator.rs
+++ src/world/generator.rs
@@ -5,7 +5,10 @@ use na::{Translation3, Vector3};
 use rand::Rng;
 use std::collections::LinkedList;
 use std::path::Path;
-
+// here are parts not to use in future
+pub fn move_spaceship(plane: &mut SceneNode, x: f32, y: f32, z: f32) {
+    plane.append_translation(&Translation3::new(x, y, z));
+}
 pub fn create_spaceship(window: &mut Window) -> SceneNode {
     let space_ship_obj = Path::new("./src/resources/spaceship/statek.obj");
     let mut space_ship = window.add_obj(
@@ -13,8 +16,8 @@ pub fn create_spaceship(window: &mut Window) -> SceneNode {
         &space_ship_obj,
         Vector3::new(0.2, 0.2, 0.2),
     );
-    space_ship.append_translation(&Translation3::new(0.0, -1.3, 4.0));
-    // space_ship.add_rotation_in_axis(1.0, 'y');
+    space_ship.append_translation(&Translation3::new(0.0, -1.5, 4.0));
+    space_ship.add_rotation_in_axis(3.15, 'y');
     let metal_texture = include_bytes!("../resources/textures/metal_texture.png");
     space_ship.set_texture_from_memory(metal_texture, "metal_texture");
     space_ship
