fa98e6de058e5341566a606c4660ee158596a1fd
bulengerkacper <bulenger.kacper@gmail.com>
2020-12-08 10:57:03 +0100

    nothing important

--- src/world/graphic.rs
+++ src/world/graphic.rs
@@ -47,7 +47,7 @@ impl Move for SceneNode {
                 .translation
                 .vector
                 .x
-            <= 1.0
+            <= 0.8
             && self.data().local_transformation().translation.vector.x
                 - flying_object
                     .data()
@@ -55,7 +55,7 @@ impl Move for SceneNode {
                     .translation
                     .vector
                     .x
-                >= -1.0
+                >= -0.8
             && self.data().local_transformation().translation.vector.y
                 - flying_object
                     .data()
@@ -63,7 +63,7 @@ impl Move for SceneNode {
                     .translation
                     .vector
                     .y
-                <= 0.18
+                <= 0.15
             && self.data().local_transformation().translation.vector.y
                 - flying_object
                     .data()
@@ -71,7 +71,7 @@ impl Move for SceneNode {
                     .translation
                     .vector
                     .y
-                >= -0.18
+                >= -0.15
             && self.data().local_transformation().translation.vector.z
                 - flying_object
                     .data()
