150e97cbb5a96b1f20a90defd474e3b79e010fb6
bulengerkacper <bulenger.kacper@gmail.com>
2020-12-09 14:01:33 +0100

    Loading screen works!

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,14 @@ extern crate nalgebra as na;
 use crate::world::generator;
 use crate::world::graphic::Move;
 use crate::world::graphic::VisualEngine as ve;
+use kiss3d::camera::FirstPerson;
 use kiss3d::event::{Action, Key, WindowEvent};
 use kiss3d::text::Font;
 use na::{Point2, Point3};
 use rand::Rng;
 
 fn main() {
+    let font = Font::default();
     let mut game_window = ve::create_window(
         "Space killer",
         kiss3d::light::Light::StickToCamera,
@@ -17,12 +19,23 @@ fn main() {
         0.0,
         0.0,
     );
-
     let mut space_ship = generator::create_spaceship(&mut game_window);
+    let mut camera = prepare_camera();
+    game_window.draw_text(
+        "GAME LOADING. Please wait",
+        &Point2::new(400.0, 400.0),
+        120.0,
+        &font,
+        &Point3::new(10.0, 200.0, 0.0),
+    );
+    while game_window.render_with_camera(&mut camera) {
+        if space_ship.data().has_object() {  //GAME LOADING
+            break;
+        }
+    }
     let mut planets = generator::generate_plantes(50, &mut game_window);
     let mut rng = rand::thread_rng();
     let mut speed_of_plantes: f32 = 0.00;
-    let font = Font::default();
     while game_window.render() {
         for planet in &mut planets {
             planet.add_rotation_in_axis(rng.gen_range(0.0, 0.01), 'y');
@@ -48,7 +61,7 @@ fn main() {
                     space_ship.add_rotation_in_axis(-0.1, 'z');
                 }
                 WindowEvent::Key(Key::Space, Action::Press, _) => {
-                    speed_of_plantes*=1.0001;
+                    speed_of_plantes *= 1.0001;
                 }
                 WindowEvent::Key(Key::L, Action::Press, _) => {
                     quit::with_code(1);
@@ -83,4 +96,11 @@ fn main() {
             );
         }
     }
+}
+
+fn prepare_camera() -> FirstPerson {
+    let eye = Point3::new(0.0, 0.0, 0.0);
+    let at = Point3::origin();
+    let mut _first_person = FirstPerson::new(eye, at);
+    _first_person
 }
